adce9b20e22a7817fd3bb0a2aea222d0c0a9380a
s21015 <s21015@std.it-college.ac.jp>
2022-06-21 14:49:37 +0900

    Initial commit

---
+++ src/main/kotlin/jp/ac/it_college/std/s21015/news_manager/NewsReleaseManagerApplication.kt
@@ -0,0 +1,11 @@
+package jp.ac.it_college.std.s21015.news_manager
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class NewsReleaseManagerApplication
+
+fun main(args: Array<String>) {
+    runApplication<NewsReleaseManagerApplication>(*args)
+}
